a12f0115b59e4e5ba2a1148dde2b1e29c2185aaf
hongjiayang920127 <132018536+hongjiayang920127@users.noreply.github.com>
2025-03-13 05:48:46 +0800

    添加mgmt-frontend项目

---
+++ mgmt-frontend/vite.config.js
@@ -0,0 +1,20 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      },
+      '/socket.io': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        ws: true
+      }
+    }
+  }
+})
